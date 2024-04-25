30aad8b2a6cfa05ad2859d65a2b012a22326ddec
ipicca <piccaignacio@gmail.com>
2024-04-25 01:31:32 -0300

    Modify function name

--- vars/sendEmails.groovy
+++ vars/sendEmails.groovy
@@ -1,6 +1,6 @@
 echo "test sendEmails"
 
-def sendEmails(String CLIENTE, String JOB_NAME, String BUILD_NUMBER, String MAILS) {
+def call(String CLIENTE, String JOB_NAME, String BUILD_NUMBER, String MAILS) {
     emailext subject: "${CLIENTE} ERROR - ${JOB_NAME}-${BUILD_NUMBER}",
               body: "<html><body><p style='color:red;'>El pipeline ${JOB_NAME} ha fallado. Revisar la consola de Jenkins para más detalles.</p></body></html>",
               mimeType: 'text/html',
