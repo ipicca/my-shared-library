af3eb7db19fbfbae47ca0e5991ba0aea397f4c75
ipicca <piccaignacio@gmail.com>
2024-04-25 00:36:03 -0300

    Added folder structure for scripts and jenkinsfiles

---
+++ jenkinsfiles/pipeline-testMails.groovy
@@ -0,0 +1,33 @@
+@Library('my-shared-library') _
+
+pipeline {
+    agent any
+
+    stages {
+        stage('TEST') {
+            steps {
+                bat "123"
+            }
+        }
+    }
+    post {
+        always {
+            echo 'Finalizado.'
+        }
+        success {
+            echo 'Exitoso.'
+        }  
+        failure {
+
+            sendEmails(env.CLIENTE, env.JOB_NAME, env.BUILD_NUMBER, env.MAILS)
+
+            echo 'Fallo.'
+        }
+        unstable {
+            echo 'Inestable!'
+        }
+        changed {
+            echo 'El estado ha cambiado.'
+        }
+    }
+}
--- src/sendEmails.groovy
+++ src/sendEmails.groovy
@@ -1,4 +1,4 @@
-def enviarMail(String cliente, String jobName, String buildNumber, String mails) {
+def sendEmails(String cliente, String jobName, String buildNumber, String mails) {
     emailext subject: "${cliente} ERROR - ${jobName}-${buildNumber}",
               body: "<html><body><p style='color:red;'>El pipeline ${jobName} ha fallado. Revisar la consola de Jenkins para más detalles.</p></body></html>",
               mimeType: 'text/html',
