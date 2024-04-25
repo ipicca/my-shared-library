952505e90caea261f5589456f43fb1646ca965c8
ipicca <piccaignacio@gmail.com>
2024-04-25 01:27:49 -0300

    Add test

--- jenkinsfiles/pipeline-testMails.groovy
+++ jenkinsfiles/pipeline-testMails.groovy
@@ -19,6 +19,7 @@ pipeline {
         }  
         failure {
 
+            echo "testing mails"
             sendEmails(env.CLIENTE, env.JOB_NAME, env.BUILD_NUMBER, env.MAILS)
 
             echo 'Fallo.'
--- vars/sendEmails.groovy
+++ vars/sendEmails.groovy
@@ -1,3 +1,5 @@
+echo "test sendEmails"
+
 def sendEmails(String CLIENTE, String JOB_NAME, String BUILD_NUMBER, String MAILS) {
     emailext subject: "${CLIENTE} ERROR - ${JOB_NAME}-${BUILD_NUMBER}",
               body: "<html><body><p style='color:red;'>El pipeline ${JOB_NAME} ha fallado. Revisar la consola de Jenkins para más detalles.</p></body></html>",
