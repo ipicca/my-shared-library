5c1362e4bf98c3ad53b266e475093076e84ba56f
ipicca <piccaignacio@gmail.com>
2024-04-24 23:45:59 -0300

    Add sendEmail.groovy file for shared library

---
+++ sendEmails.groovy
@@ -0,0 +1,7 @@
+def enviarMail(String cliente, String jobName, String buildNumber, String mails) {
+    emailext subject: "${cliente} ERROR - ${jobName}-${buildNumber}",
+              body: "<html><body><p style='color:red;'>El pipeline ${jobName} ha fallado. Revisar la consola de Jenkins para más detalles.</p></body></html>",
+              mimeType: 'text/html',
+              to: "${mails}",
+              attachLog: true
+}
